b3c8b6f109305291b1b0b81aded08ad82ef73a06
zmiyka <povazhnyirostyslav@gmail.com>
2025-04-05 11:50:13 +0100

    Update areafinder.py

--- areafinder.py
+++ areafinder.py
@@ -1,6 +1,7 @@
 def calculate_cyrcle(radius):
     """Формула кола: π * (r * r)"""
-    return radius * 3.14
+    area = radius * 3.14 * 2
+    return(area)
 
 def calculate_parallelogram_area(side: float, height: float):
     """Обцислює площу паралелограма за стороною та висотою проведеною до неї"""
